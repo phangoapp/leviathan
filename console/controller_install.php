80df4a59585b1221466f193aaeaa6a5fbdf84223
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-09-21 04:46:43 +0200

    Fix in installing

--- console/controller_install.php
+++ console/controller_install.php
@@ -20,10 +20,43 @@ function InstallConsole()
     
     $ssh_pass=Utils::generate_random_password(20);
     
-    pcntl_exec("ssh-keygen -t rsa -P \"".$ssh_pass."\" -f \"".ConfigTask::$ssh_key_priv[0]."\"");
+    exec_command("ssh-keygen -t rsa -P \"".$ssh_pass."\" -f \"".ConfigTask::$ssh_key_priv[0]."\"", 'Error:cannot create the ssh key');
 
     echo "Leviathan module installed...\n";
 
 }
+
+function exec_command($command, $error_text)
+{
+    $descriptorspec = [
+        0 => array("pipe", "r")  
+    ];
+    
+    $process = proc_open($command, $descriptorspec, $pipes);
+
+    if(is_resource($process)) 
+    {
+        
+        while($text=fread($pipes[0], 4096))
+        {
+            
+            echo $text;
+            
+        }
+        
+    }
+    else
+    {
+        
+        echo $error_text."\n";
+        exit(1);
+        
+    }
+
+    proc_close($process);
+
+    
+}
+
 
 ?>
