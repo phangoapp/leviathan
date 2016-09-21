e93598bac5a35bd1df49d4cf618c72a14e280c26
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-09-21 03:14:34 +0200

    Fixes in config.php.sample

---
+++ console/controller_install.php
@@ -0,0 +1,22 @@
+<?phhp
+
+<?php
+
+use PhangoApp\PhaModels\Webmodel;
+use PhangoApp\Leviathan;
+use PhangoApp\Leviathan\ConfigTask;
+
+gc_enable();
+gc_collect_cycles();
+
+Webmodel::load_model('vendor/phangoapp/leviathan/models/tasks');
+Webmodel::load_model('vendor/phangoapp/leviathan/models/servers');
+
+function InstallConsole($task_id)
+{
+
+    echo "All installed...";
+
+}
+
+?>
