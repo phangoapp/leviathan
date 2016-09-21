7688b3d07669b91abc76ae83135de722897ce6d8
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-09-21 19:03:18 +0200

    Fixes

--- install/install.php
+++ install/install.php
@@ -6,6 +6,10 @@ use PhangoApp\Leviathan\ConfigTask;
 use PhangoApp\PhaUtils\Utils;
 use PhangoApp\PhaRouter\Routes;
 
+include('vendor/autoload.php');
+
+
+
 Webmodel::load_model('vendor/phangoapp/leviathan/models/tasks');
 Webmodel::load_model('vendor/phangoapp/leviathan/models/servers');
 
@@ -24,6 +28,8 @@ $config=file_get_contents('vendor/phangoapp/leviathan/settings/config.php');
 $config=str_replace('ConfigTask::$ssh_key_password=[\'password\'];', 'ConfigTask::$ssh_key_password=[\''.$ssh_pass.'\'];', $config);
 
 $config=str_replace('ConfigTask::$url_monit=\'http://host/index.php/leviathan/monit\';', 'ConfigTask::$url_monit=\'http://'.gethostname().Routes::$root_url.'index.php/leviathan/monit\';', $config);
+
+$mail=readline('Email where send notifies: ');
 
 file_put_contents('vendor/phangoapp/leviathan/settings/config.php', $config);
 
