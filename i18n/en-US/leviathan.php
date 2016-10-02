ab723eca6d8b48548a7e9e657c6ee2d67f0cd3b4
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-10-03 01:11:52 +0200

    Fixes in 18n files

--- i18n/en-US/leviathan.php
+++ i18n/en-US/leviathan.php
@@ -2,6 +2,8 @@
 
 use PhangoApp\PhaI18n\I18n;
 
+I18n::$lang['phangoapp/leviathan']['servers_down']='WARNING:  SERVERS ARE DOWN!';
+
 I18n::$lang['phangoapp/leviathan']['subgroups']='Subgroups';
 
 I18n::$lang['phangoapp/leviathan']['choose_server']='Choose server';
@@ -10,7 +12,7 @@ I18n::$lang['phangoapp/leviathan']['os_codename']='Operating system';
 
 I18n::$lang['phangoapp/leviathan']['disable_root_password']='Disable root password';
 
-I18n::$lang['phangoapp/leviathan']['clean_gcc']='Clean build dependencies? By default clean compilers and others tools that normally you don\'t you need in your server';
+I18n::$lang['phangoapp/leviathan']['clean_gcc']='Clean build dependencies? By default clean compilers and others tools that normally you don't you need in your server';
 
 I18n::$lang['phangoapp/leviathan']['principal_group']='Principal group of server';
 
@@ -30,6 +32,8 @@ I18n::$lang['phangoapp/leviathan']['update_servers']='Make task in selected serv
 
 I18n::$lang['phangoapp/leviathan']['update_servers_os']='Update server using the native package manager';
 
+I18n::$lang['phangoapp/leviathan']['dashboard']='Dashboard';
+
 I18n::$lang['phangoapp/leviathan']['groups_servers']='Servers groups';
 
 I18n::$lang['phangoapp/leviathan']['os_servers']='Os Servers';
@@ -44,11 +48,29 @@ I18n::$lang['phangoapp/leviathan']['maketask']='Make task in servers';
 
 I18n::$lang['phangoapp/leviathan']['leviathan']='Leviathan';
 
-I18n::$lang['phangoapp/leviathan']['passwords_doesnt_match']='Passwords doesn\'t match';
+I18n::$lang['phangoapp/leviathan']['passwords_doesnt_match']='Passwords doesn't match';
 
 I18n::$lang['phangoapp/leviathan']['add_server']='Add server';
 
 I18n::$lang['phangoapp/leviathan']['info']='Info';
+
+I18n::$lang['phangoapp/leviathan']['number_of_servers']='Number of servers';
+
+I18n::$lang['phangoapp/leviathan']['number_of_cpus']='Number of total cpu cores';
+
+I18n::$lang['phangoapp/leviathan']['hosts_down']='HOSTS DOWN';
+
+I18n::$lang['phangoapp/leviathan']['view_down_servers']='View down servers';
+
+I18n::$lang['phangoapp/leviathan']['hosts_loaded']='HOSTS HEAVILY LOADED';
+
+I18n::$lang['phangoapp/leviathan']['view_heavy_servers']='View heavily loaded servers';
+
+I18n::$lang['phangoapp/leviathan']['total_cpu_use_average']='Total cpu use average';
+
+I18n::$lang['phangoapp/leviathan']['servers_load']='Servers load';
+
+I18n::$lang['phangoapp/leviathan']['total_net_use']='Total network use';
 
 I18n::$lang['phangoapp/leviathan']['graphs_of']='Servers Graphs';
 
@@ -56,9 +78,11 @@ I18n::$lang['phangoapp/leviathan']['num_servers']='Number of servers';
 
 I18n::$lang['phangoapp/leviathan']['completed_tasks']='Completed tasks';
 
-I18n::$lang['phangoapp/leviathan']['loading_servers']='Loading servers...';
+I18n::$lang['phangoapp/leviathan']['making_tasks']='Making tasks...';
 
 I18n::$lang['phangoapp/leviathan']['server_log']='Server log';
+
+I18n::$lang['phangoapp/leviathan']['error_review_log']='Error: please, review the log';
 
 I18n::$lang['phangoapp/leviathan']['task_progress']='Task progress';
 
@@ -68,19 +92,5 @@ I18n::$lang['phangoapp/leviathan']['go_back']='Go back';
 
 I18n::$lang['phangoapp/leviathan']['all_servers']='All servers';
 
-I18n::$lang['phangoapp/leviathan']['disk']='Disk';
-
-I18n::$lang['phangoapp/leviathan']['free']='free';
-
-I18n::$lang['phangoapp/leviathan']['cpu_number']='Cpu number';
-
-I18n::$lang['phangoapp/leviathan']['total_cpu_idle']='Total cpu idle';
-
-I18n::$lang['phangoapp/leviathan']['memory_used']='Memory used';
-
-I18n::$lang['phangoapp/leviathan']['domains']='Domains';
-
-I18n::$lang['phangoapp/leviathan']['used']='used';
-
-I18n::$lang['phangoapp/leviathan']['cached']='cached';
+I18n::$lang['phangoapp/leviathan']['make_task_servers']='Make task in selected servers';
 
--- i18n/es-ES/leviathan.php
+++ i18n/es-ES/leviathan.php
@@ -2,6 +2,8 @@
 
 use PhangoApp\PhaI18n\I18n;
 
+I18n::$lang['phangoapp/leviathan']['servers_down']='Servidores caidos';
+
 I18n::$lang['phangoapp/leviathan']['subgroups']='Subgrupos';
 
 I18n::$lang['phangoapp/leviathan']['choose_server']='Elegir servidor';
@@ -30,6 +32,8 @@ I18n::$lang['phangoapp/leviathan']['update_servers']='Actualizar servidores sele
 
 I18n::$lang['phangoapp/leviathan']['update_servers_os']='Actualizar servidores usando el package manager por defecto';
 
+I18n::$lang['phangoapp/leviathan']['dashboard']='Dashboard';
+
 I18n::$lang['phangoapp/leviathan']['groups_servers']='Grupos de servidores';
 
 I18n::$lang['phangoapp/leviathan']['os_servers']='Sistemas operativos de servidores';
@@ -50,15 +54,35 @@ I18n::$lang['phangoapp/leviathan']['add_server']='Añadir servidor';
 
 I18n::$lang['phangoapp/leviathan']['info']='Información';
 
+I18n::$lang['phangoapp/leviathan']['number_of_servers']='Numero de servidores';
+
+I18n::$lang['phangoapp/leviathan']['number_of_cpus']='Numero total de nucleos de CPU';
+
+I18n::$lang['phangoapp/leviathan']['hosts_down']='SERVIDORES CAIDOS';
+
+I18n::$lang['phangoapp/leviathan']['view_down_servers']='Ver servidores caídos';
+
+I18n::$lang['phangoapp/leviathan']['hosts_loaded']='Servidores demasiado cargados';
+
+I18n::$lang['phangoapp/leviathan']['view_heavy_servers']='Ver servidores con exceso de carga';
+
+I18n::$lang['phangoapp/leviathan']['total_cpu_use_average']='Media total de uso de CPU';
+
+I18n::$lang['phangoapp/leviathan']['servers_load']='Servidores cargados';
+
+I18n::$lang['phangoapp/leviathan']['total_net_use']='Uso total de red';
+
 I18n::$lang['phangoapp/leviathan']['graphs_of']='Gráficas de servidores';
 
 I18n::$lang['phangoapp/leviathan']['num_servers']='Número de servidores';
 
 I18n::$lang['phangoapp/leviathan']['completed_tasks']='Tareas completadas';
 
-I18n::$lang['phangoapp/leviathan']['loading_servers']='Cargando servidores...';
+I18n::$lang['phangoapp/leviathan']['making_tasks']='Hacer tareas...';
 
 I18n::$lang['phangoapp/leviathan']['server_log']='Log de servidor';
+
+I18n::$lang['phangoapp/leviathan']['error_review_log']='Error: por favor, revise el log';
 
 I18n::$lang['phangoapp/leviathan']['task_progress']='Progreso de tareas';
 
@@ -68,19 +92,5 @@ I18n::$lang['phangoapp/leviathan']['go_back']='Regresar';
 
 I18n::$lang['phangoapp/leviathan']['all_servers']='Todos los servidores';
 
-I18n::$lang['phangoapp/leviathan']['disk']='Disco';
-
-I18n::$lang['phangoapp/leviathan']['free']='libre';
-
-I18n::$lang['phangoapp/leviathan']['cpu_number']='Numero de cpus';
-
-I18n::$lang['phangoapp/leviathan']['total_cpu_idle']='Ocupación de cpu';
-
-I18n::$lang['phangoapp/leviathan']['memory_used']='Memoria usada';
-
-I18n::$lang['phangoapp/leviathan']['domains']='Dominios';
-
-I18n::$lang['phangoapp/leviathan']['used']='usada';
-
-I18n::$lang['phangoapp/leviathan']['cached']='en caché';
+I18n::$lang['phangoapp/leviathan']['make_task_servers']='Hacer tarea en servidores seleccionados';
 
